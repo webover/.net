1358950815e612b1de495f8dbc8e86ce53a7d4a6
Adrian Paduraru <adrian.paduraru@traveltech.dotw.com>
2019-11-19 08:31:07 +0200

    remove duplicates

---
+++ Homework/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework
+{
+    class Duplicates
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Your string is: " + input);
+
+            string withoutDuplicates = removeDuplicate(input);
+
+            Console.WriteLine("String without duplicates will be: " + withoutDuplicates);
+        }
+
+        static string removeDuplicate(string input)
+        {
+            string result = "";
+
+            foreach(char value in input)
+            {
+                if(result.IndexOf(value) == -1)
+                {
+                    result += value;
+                }
+            }
+
+            return result;
+        }
+    }
+}
