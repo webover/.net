b0c004a450e2e4c88f4a2224d12e7005f9c5c40e
Adrian Paduraru <adrian.paduraru@traveltech.dotw.com>
2019-11-20 14:10:55 +0200

    if no words

--- LastWordLength/Program.cs
+++ LastWordLength/Program.cs
@@ -11,9 +11,15 @@ namespace LastWordLength
             string input = Console.ReadLine().Trim();
             int stringLength = input.Length;
 
-            string[] words = input.Split(" ");
+            if(stringLength > 0)
+            {
+                string[] words = input.Split(" ");
 
-            Console.WriteLine("Last word is '{0}' and it has a {1} letters length", words[words.Length - 1], words[words.Length - 1].Length);
+                Console.WriteLine("Last word is '{0}' and it has a {1} letters length", words[words.Length - 1], words[words.Length - 1].Length);
+            } else
+            {
+                Console.WriteLine("No words were entered.");
+            }            
         }
     }
 }
