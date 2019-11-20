5e96c6a6553f6169b08e6de414d4933a2089f238
Adrian Paduraru <adrian.paduraru@traveltech.dotw.com>
2019-11-20 20:57:23 +0200

    Duplicate Linked List

---
+++ DuplicatesLinkedList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DuplicatesLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] duplicatesArray = { 1, 2, 3, 4, 4, 5, 5, 5, 6, 7, 8, 8, 9, 1, 2, 3, 4, 5, 6 };
+            LinkedList<int> list = new LinkedList<int>(duplicatesArray);
+
+            var startNode = list.First;
+
+            while (startNode != null)
+            {
+                var nextNode = startNode;
+
+                Console.WriteLine("Checking for " + startNode.Value);
+
+                while (nextNode.Next != null)
+                {
+                    if (nextNode.Next.Value.Equals(startNode.Value))
+                    {
+                        list.Remove(nextNode.Next);
+                    }
+                    else
+                    {
+                        nextNode = nextNode.Next;
+                    }
+                }
+
+                startNode = startNode.Next;
+            }
+
+            Console.WriteLine("initial list was {0} without duplicates is {1}", String.Join("", duplicatesArray), String.Join("", list));
+        }
+    }
+}
--- LastWordLength/Program.cs
+++ LastWordLength/Program.cs
@@ -11,15 +11,16 @@ namespace LastWordLength
             string input = Console.ReadLine().Trim();
             int stringLength = input.Length;
 
-            if(stringLength > 0)
+            if (stringLength > 0)
             {
                 string[] words = input.Split(" ");
 
                 Console.WriteLine("Last word is '{0}' and it has a {1} letters length", words[words.Length - 1], words[words.Length - 1].Length);
-            } else
+            }
+            else
             {
                 Console.WriteLine("No words were entered.");
-            }            
+            }
         }
     }
 }
